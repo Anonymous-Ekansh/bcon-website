7032e0ff6dbe1934ea05580b8b8eca0d7c7f9e57
Ekansh <ekanshjain329@gmail.com>
2026-09-11 16:29:29 +0530

    Increase logo size in hero section for mobile

--- src/components/index/hero-section.tsx
+++ src/components/index/hero-section.tsx
@@ -258,7 +258,7 @@ const HeroSection = () => {
           <Image 
             src="/images/snioe-inspiria.png" 
             alt="SNIoE &times; Inspiria Presents" 
-            h={{ base: "50px", md: "120px", lg: "150px", xl: "180px" }}
+            h={{ base: "80px", md: "120px", lg: "150px", xl: "180px" }}
             maxW="100%"
             objectFit="contain"
             objectPosition={{ base: "center", lg: "left center" }}
